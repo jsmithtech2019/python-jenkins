e863482582748fea6d30aff8efec6e060ab2a7d2
Jack Smith <jsmithtech2019@gmail.com>
2020-04-08 13:08:34 -0500

    Do things better, faster, stronger

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def help(unused):
 # Post a relevant gif from Giphy
 def giphy(text):
     print('Text: ' + text)
-    search = encodeQuery(text[len('/giphy'):])
+    search = encodeQuery(text[len('/giphy '):])
     print('Search: ' + search)
     imgURL = 'https://api.giphy.com/v1/gifs/search?api_key={}&q={}'.format(giphy_api_key, search)
 
